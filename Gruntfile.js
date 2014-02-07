a300ced4edba4843e152893112a3a39f947e56d5
David Konsumer <konsumer@jetboystudio.com>
2014-02-07 12:27:13 -0800

    minify in grunt & use google pages as CDN in examples

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,29 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		'gh-pages': {
 			'options': {
-				'base': 'examples'
+				'base': '.grunt/temp'
 			},
 			'src': ['**']
 		},
+		'copy': {
+			'page':{
+				'files':[
+					{expand: true, src: ['examples/**'], dest: '.grunt/temp/'},
+				]
+			}
+		},
+		'uglify': {
+			'page': {
+				'files': {
+					'.grunt/temp/cardflow.min.js': ['cardflow.js']
+				}
+			}
+		}
 		'connect': {
 			'server': {
 				'options': {
 					'port': 9001,
+					'hostname': '*',
         			'base': 'examples',
         			'keepalive':true
 				},
@@ -18,10 +33,17 @@ module.exports = function(grunt) {
 		}
 	});
 
+
+	// copy files to temp dir
+	// uglify coverflow.js
+	// push to gh-pages
+
 	grunt.loadNpmTasks('grunt-gh-pages');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.registerTask('page', 'Publish examples to github pages', ['gh-pages']);
+	grunt.registerTask('page', 'Publish examples to github pages', ['copy:page', 'uglify:page', 'gh-pages']);
 	grunt.registerTask('server', 'Run local static webserver for examples', ['connect']);
 	grunt.registerTask('serve', ['connect:server']);
 	grunt.registerTask('default', ['page']);
